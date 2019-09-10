16419c1fb88885b50281f93bdbc3922761faf7ca
Adrian <adde.a@hotmail.com>
2019-09-10 14:42:50 +0200

    Early draft of an attempt of a try on having an inspiring About page

--- src/About.js
+++ src/About.js
@@ -10,16 +10,24 @@ class About extends Component {
     return(
    	  <div>
       	<p style={styles.para}>
-            Lorem ipsum dolor sit amet, consectetur <Emph word="adipiscing"/> elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Justo donec enim diam vulputate ut. Nunc sed blandit libero volutpat sed. 
-            Suspendisse sed nisi lacus sed viverra tellus. Amet luctus venenatis lectus 
-            magna fringilla urna. Ullamcorper <Emph word="dignissim"/> cras tincidunt lobortis feugiat. 
-            Elementum facilisis leo vel fringilla est. Eget lorem dolor sed viverra ipsum 
-            nunc aliquet bibendum enim. Ut venenatis tellus in metus. Egestas sed tempus 
-            urna et pharetra pharetra. <br/>
-            <Emph word="Sapien"/> eget mi proin sed libero. Elementum curabitur 
-            vitae nunc sed velit.
+            I'm <Emph word="Adrian Andersson"/>, a M.Sc. Student in Media Technology and Engineering
+            at the university of Linköping. Originally from <Emph word="Nässjö"/>, Småland,
+            my interest for tech took me to Norrköping to study and become an engineer.
+            I am currently on my <Emph word="fourth year"/> and have just begun my masters. At the moment
+            I have no special interest in any particular field, I'm still trying to decide
+            which field to pursue. Media technology offers a strong foundation in programming,
+            mathematics, and physics. On top of that media technology students excel in computer
+            graphics, imaging, and visualization. 
+            <br/>
+            <br/>
+            I have always been a fast learner and is <Emph word="eager to learn"/> new things, because 
+            of this I have been successful in school and have achieved high grades in most 
+            of my courses. Grades only matter that much when applying for a job which is why 
+            I also created this portfolio to showcase different projects.
+            <br/>
+            <br/>
+            Special thanks to my wonderful and inspiring girlfriend for always 
+            supporting me <Emph word="<3"/>.
         </p>
       </div>
     );
@@ -33,7 +41,7 @@ const styles = {
       fontSize: 'calc((2vh + 2vw)/2)',
       textAlign: 'left',
       float: 'left',
-      width: '75%',
+      width: '60%',
     },
 
   highlight: {
