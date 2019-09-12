85a01fc91b942b7ad9ad6c2582d131e3187b360d
Adrian Andersson <Adrian Andersson>
2019-09-12 18:09:10 +0200

    Test 3

--- src/Projects.js
+++ src/Projects.js
@@ -11,35 +11,112 @@ class Home extends Component {
     return(
     	 <ul id="scrollstyle" style={styles.container}>
           <li style={styles.item}>
-              Vacuum Hero
+              <h1>Vacuum Hero</h1>
+              <p>
+                A racing game where the player controls a vacuum machine
+                to collect exams in order to get points. The game takes
+                place in a 3D model of Täppan in campus Norrköping.
+              </p>
+              <p>
+                
+                    {code}
+                
+                
+                    {preview}
+                
+              </p>
           </li>
           
           <li style={styles.item}>
-              Biljard Simulation
+              <h1>Evari</h1>
+              <p>
+                A RTS-styled VR game made in Unity.
+              </p>
+              <p>
+                
+                    {code}
+                
+                
+                    {preview}
+                
+              </p>
           </li>
           
           <li style={styles.item}>
-              Monte-Carlo Ray tracer
+              <h1>Monte-Carlo Ray tracer</h1>
+              <p>
+                An ongoing project to render a simple scene
+                containing Lambertian and Oren-Nayar reflectors in
+                addition to transparent and perfectly reflecting objects.
+              </p>
+              <p>
+                
+                    {code}
+                
+                
+                    {preview}
+                
+              </p>
+          </li>
+          
+          <li style={styles.item}> 
+              <h1>Handwritten Number Identification</h1>
+              <p>
+                A MatLab program made to identify handwritten
+                numbers using different algorithms.
+              </p>
+              <p>
+                <a href='https://github.com/AddeAndersson/NumberIdentification'>
+                    {code}
+                </a>
+                
+                    {preview}
+                
+              </p>
           </li>
           
           <li style={styles.item}>
-              Handwritten Number Identification
+              <h1>Lego Searcher</h1>
+              <p>
+                A website to browse through a database of lego pieces
+                in order to see which set contains that piece.
+              </p>
+              <p>
+                <a href='https://github.com/AddeAndersson/LegoSearcher'>
+                    {code}
+                </a>
+                
+                    {preview}
+                
+              </p>
           </li>
-          
+
           <li style={styles.item}>
-              Lego Searcher
+              <h1>Biljard Simulation</h1>
+              <p>
+                A short video as a result of simulating the initial
+                hit in a game of biljard.
+              </p>
+              <p>
+                <a href='https://github.com/AddeAndersson/TNM085'>
+                    {code}
+                </a>
+                
+                    {preview}
+                
+              </p>
           </li>
           
           <li style={styles.item}>
               <h1>Online Portfolio</h1>
               <p>You are looking at it.</p>
               <p>
-                <a href='https://github.com/AddeAndersson'>
+                <a href='https://github.com/AddeAndersson/portfolio'>
                     {code}
                 </a>
-                <a href='https://youtube.com/'>
+                
                     {preview}
-                </a>
+                
               </p>
           </li>
        </ul>
