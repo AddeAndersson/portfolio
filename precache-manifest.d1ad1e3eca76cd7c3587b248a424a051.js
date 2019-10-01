7f4eddf5511b301ec469d9f74c8f8d79e6835945
Adrian Andersson <Adrian Andersson>
2019-10-01 09:04:35 +0200

    Updates

--- precache-manifest.d1ad1e3eca76cd7c3587b248a424a051.js
+++ precache-manifest.d1ad1e3eca76cd7c3587b248a424a051.js
@@ -1,5 +1,9 @@
 self.__precacheManifest = [
   {
+    "revision": "0f152645b30f363ae397967e72ecb971",
+    "url": "/portfolio/static/media/wallhaven-4yjxkg.0f152645.jpg"
+  },
+  {
     "revision": "e76fea2adc00c6904423b75bf5c8f021",
     "url": "/portfolio/static/media/adrianandersson.e76fea2a.pdf"
   },
@@ -8,19 +12,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "6c236dd1438acf979015",
-    "url": "/portfolio/static/js/main.6c236dd1.chunk.js"
+    "revision": "fe6ea8e9253dd2116a2d",
+    "url": "/portfolio/static/js/main.fe6ea8e9.chunk.js"
   },
   {
     "revision": "e1777eab331510a13dcc",
     "url": "/portfolio/static/js/1.e1777eab.chunk.js"
   },
   {
-    "revision": "6c236dd1438acf979015",
-    "url": "/portfolio/static/css/main.bcdad757.chunk.css"
+    "revision": "fe6ea8e9253dd2116a2d",
+    "url": "/portfolio/static/css/main.df983d86.chunk.css"
   },
   {
-    "revision": "5fb786f6eeaf0179959581667b3c4251",
+    "revision": "2a8f19d9124275471984d3ecdab2906e",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f73ba617d76b3122fe5447b48e415136.js"
+  "/portfolio/precache-manifest.d1ad1e3eca76cd7c3587b248a424a051.js"
 );
 
 workbox.clientsClaim();
