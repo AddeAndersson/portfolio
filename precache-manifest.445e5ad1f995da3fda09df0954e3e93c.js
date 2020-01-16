28cc0e537849c87f1d17d077eb3b55715af44de4
Adrian Andersson <Adrian Andersson>
2020-01-16 21:19:49 +0100

    Updates

---
+++ precache-manifest.445e5ad1f995da3fda09df0954e3e93c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2a4fcfd72d4462c52544ff6281f4bff",
+    "url": "/portfolio/static/media/linkedin.a2a4fcfd.svg"
+  },
+  {
+    "revision": "bb69c9700f7c12867ee937b5177a89d9",
+    "url": "/portfolio/static/media/github.bb69c970.svg"
+  },
+  {
+    "revision": "8441acadad2e0e0b65bc5d497a7ee22d",
+    "url": "/portfolio/static/media/adrianandersson.8441acad.pdf"
+  },
+  {
+    "revision": "22d8fecc06f3b2cd56376b7aa9b299bb",
+    "url": "/portfolio/static/media/TNCG15.22d8fecc.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "47556a35be2dd1d72451",
+    "url": "/portfolio/static/js/main.47556a35.chunk.js"
+  },
+  {
+    "revision": "9002a56a171ceab88909",
+    "url": "/portfolio/static/js/1.9002a56a.chunk.js"
+  },
+  {
+    "revision": "47556a35be2dd1d72451",
+    "url": "/portfolio/static/css/main.35c2e495.chunk.css"
+  },
+  {
+    "revision": "e83225d438da05b5c9301e09f12cb48d",
+    "url": "/portfolio/index.html"
+  }
+];
